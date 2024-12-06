c4fdd4200fd583f7e3861a5d49b609a632c5f63f
Matheesha Maduwantha <124491632+Mmaduwantha@users.noreply.github.com>
2024-12-06 16:37:08 +0530

    db done

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import pool from './db.js';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
+pool.connect((err, client, release) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.stack);
+    } else {
+        console.log('Connected to PostgreSQL database successfully!');
+        release();
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Job Scraping Website!');
+});
 
 app.listen(port, () => {
     console.log(`Server started on port ${port} successfully!`);
